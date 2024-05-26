56954489db1ef894c646a1dacc9ec50451277d93
solhwi <psh50zmfhtm@naver.com>
2024-05-27 00:36:00 +0900

    2

---
+++ BotClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Text.Encodings.Web;
+using System.Web;
+
+namespace BotClient
+{
+	internal class Program
+	{
+		public enum TestCaseType
+		{
+			CompleteYujin,
+		}
+
+		private static string serverBaseUrl = "http://localhost:8000";
+		private static HttpClient client = new HttpClient();
+
+		private static Dictionary<TestCaseType, string> testCaseDictionary = new Dictionary<TestCaseType, string>()
+		{
+			{ TestCaseType.CompleteYujin, "/Kahlua?p1=완료&p2=유진" },
+		};
+
+		public static async Task Main(string[] args)
+		{
+			if (testCaseDictionary.TryGetValue(TestCaseType.CompleteYujin, out string testParameters))
+			{
+				var message = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, serverBaseUrl + testParameters));
+				string str = await message.Content.ReadAsStringAsync();
+				Console.WriteLine(str);
+			}
+		}
+	}
+}
